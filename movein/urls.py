fa5164bf7a4e6f38c47c3faaaef5099f82938768
snowP_ <jverceluz@gbox.adnu.edu.ph>
2024-09-30 21:24:46 +0800

    chore: add project BBL_MoveIn directory

---
+++ movein/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', index, name="index"),
+    
+    # OWNER URLS
+    path('owner/rooms', l_room, name="tenant_room"),
+    path('owner/announcements', l_announcement, name="landlord_announcement"),
+    path('owner/announcements/create', announcement_view, name="createAnnouncement"),
+
+    # TENANT URLS
+    path('tenant/myRoom', t_myRoom, name="myRoom"),
+    
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
